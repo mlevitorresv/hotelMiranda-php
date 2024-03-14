96c9ba18857fe881816cbbc5ce493bbe1402c21b
mlevitorresv <levitorres66@gmail.com>
2024-03-14 11:40:45 +0100

    RM: header and footer in blade.php

--- views/roomDetails.blade.php
+++ views/roomDetails.blade.php
@@ -12,34 +12,6 @@
     <title>Room Details</title>
 </head>
 <body>
-    <header class="header">
-        <div class="header__all">
-            <div class="header__all--left">
-                <img src="./media/burgerIcon.png" alt="menu icon" class="header__all--left--burger" id="burgerIcon"/>
-                <img src="./media/crossBurger.png" alt="cross icon" class="header__all--left--cross" id="crossBurgerIcon"/>                
-                <a href="./index.html" class="header__all--left--logo">
-                    <h1 class="header__all--left--logo--H">H</h1>
-                    <h1 class="header__all--left--logo--name">Hotel<br />Miranda</h1>
-                </a>
-            </div>
-
-            <div class="header__all--menu" id="menu">
-                <ul class="header__all--menu--items">
-                    <a href="./about.html" class="header__all--menu--items--link"><li class="header__all--menu--items--about">About Us</li></a>
-                    <a href="./rooms.html" class="header__all--menu--items--link"><li class="header__all--menu--items--rooms">Rooms</li></a>
-                    <a href="./offers.html" class="header__all--menu--items--link"><li class="header__all--menu--items--offers">Offers</li></a>
-                    <a href="./contact.html" class="header__all--menu--items--link"><li class="header__all--menu--items--contact">Contact</li></a>
-                </ul>
-            </div>
-
-            <div class="header__all--right">
-                <img src="./media/personIcon.png" alt="person icon" class="header__all--right--person" />
-                <img src="./media/lensIcon.png" alt="lens icon"  class="header__all--right--lens"/>
-            </div>
-        </div>
-    </header>
-
-
     <main class="main__details">
         <div class="main__details--intro">
             <p class="main__details--intro--slogan">THE ULTIMATE LUXURY</p>
@@ -194,91 +166,5 @@
             </div>
         </div>
     </main>
-
-
-    <footer class="footer">
-        <div class="footer__us">
-
-            <div class="footer__us--logo">
-                <p class="footer__us--logo--H">H</p>
-                <p class="footer__us--logo--name"><span class="footer__us--logo--name--strong">HOTEL</span><br />MIRANDA</p>
-            </div>
-            <p class="footer__us--desc">
-                Lorem ipsum dolor sit amet, consect etur adipisicing elit, sed doing eius mod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitat ion ullamco laboris nisi.
-            </p>
-            <div class="footer__us--social">
-                <div class="footer__us--social--element">
-                    <img src="./media/facebook.svg" alt="facebook  icon" class="footer__us--social--element--icon">
-                </div>
-                <div class="footer__us--social--element">
-                    <img src="./media/twitter.svg" alt="twitter icon" class="footer__us--social--element--icon">
-                </div>
-                <div class="footer__us--social--element">
-                    <img src="./media/behance.svg" alt="behance icon" class="footer__us--social--element--icon">
-                </div>
-                <div class="footer__us--social--element">
-                    <img src="./media/linkedin.svg" alt="linkedin icon" class="footer__us--social--element--icon">
-                </div>
-                <div class="footer__us--social--element">
-                    <img src="./media/yt.svg" alt="youtube icon" class="footer__us--social--element--icon">
-                </div>
-            </div>
-
-        </div>
-
-        <div class="footer__services">
-            <h4 class="footer__services--title">Services.</h4>
-            <div class="footer__services--elements">
-                <p class="footer__services--elements--element">+ Resturent & Bar</p>
-                <p class="footer__services--elements--element">+ Swimming Pool</p>
-                <p class="footer__services--elements--element">+ Wellness & Spa</p>
-                <p class="footer__services--elements--element">+ Restaurant</p>
-                <p class="footer__services--elements--element">+ Conference Room</p>
-                <p class="footer__services--elements--element">+ Coctail Party House</p>
-                <p class="footer__services--elements--element">+ Gaming Zone</p>
-                <p class="footer__services--elements--element">+ Marrige Party</p>
-                <p class="footer__services--elements--element">+ Party Planning</p>
-                <p class="footer__services--elements--element">+ Tour Consultancy</p>
-            </div>
-        </div>
-        <div class="footer__contact">
-            <h4 class="footer__contact--title">Contact Us.</h4>
-            <div class="footer__contact--element">
-                <img src="./media/footerPhone.png" alt="phone icon" class="footer__contact--element-icon">
-                <p class="footer__contact--element--data">
-                    <span class="footer__contact--element--data--key"> Phone Number</span>
-                    <br />
-                    <span class="footer__contact--element--data--value">+34 620 46 82 40</span>
-                </p>
-            </div>
-            <div class="footer__contact--element">
-                <img src="./media/email.png" alt="email icon" class="footer__contact--element-icon">
-                <p class="footer__contact--element--data">
-                    <span class="footer__contact--element--data--key">Email Address</span>
-                    <br />
-                    <span class="footer__contact--element--data--value">levitorres66@gmail.com</span>
-                </p>
-            </div>
-            <div class="footer__contact--element">
-                <img src="./media/location.png" alt="location icon" class="footer__contact--element-icon">
-                <p class="footer__contact--element--data">
-                    <span class="footer__contact--element--data--key"> Office Address</span>
-                    <br />
-                    <span class="footer__contact--element--data--value">Princesa 31, planta 2 (28008 Madrid)</span>
-                </p>
-            </div>
-        </div>
-    </footer>
-    <div class="footer__end">
-        <p class="footer__end--copy">
-            Copyright By Manuel Leví Torres Vicente - 2023
-        </p>
-        <p class="footer__end--terms">
-            Terms of use | Privacy Environmental policy
-        </p>
-    </div>
-    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="./js/index.js"></script>
-    <script src="./js/slider.js"></script>
 </body>
 </html>
