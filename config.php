5f6d6af24b55363cc8cae0b8c955cffaa7016381
mlevitorresv <levitorres66@gmail.com>
2024-05-02 09:33:09 +0200

    tables creation removed

--- config.php
+++ config.php
@@ -27,14 +27,3 @@ $sql = "CREATE DATABASE IF NOT EXISTS mirandaphp";
 if ($conn->query($sql) === FALSE) {
     echo "Error creating database: " . $conn->error;
 }
-
-$sqlFile = '../db/db.sql';
-$sqlCommands = file($sqlFile);
-
-foreach ($sqlCommands as $sqlCommand) {
-    // Ejecuta la consulta SQL
-    if ($conn->query($sqlCommand) === FALSE) {
-        echo "Error al ejecutar la consulta: $sqlCommand <br>";
-        echo "Error: " . $conn->error . "<br>";
-    }
-}
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '669c6380f4f1ad77446809528080354c7dd36d4f',
+        'reference' => '5a46936b64abb1fcbdd525d54c93bc5ba84c0c65',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '669c6380f4f1ad77446809528080354c7dd36d4f',
+            'reference' => '5a46936b64abb1fcbdd525d54c93bc5ba84c0c65',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
