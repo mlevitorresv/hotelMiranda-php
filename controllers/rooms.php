c1563a0eb977d5bd12ade0255a53cf5708266b10
mlevitorresv <levitorres66@gmail.com>
2024-05-06 16:14:12 +0200

    reseration with error

--- controllers/rooms.php
+++ controllers/rooms.php
@@ -1,60 +1,97 @@
 <?php
-    require_once "config.php";
+require_once "config.php";
 
-    function getRooms() {
-        global $conn;
-        $query = "SELECT * FROM mirandaphp.rooms";
-        $result = $conn -> query($query);
+function getRooms()
+{
+    global $conn;
+    $query = "SELECT * FROM mirandaphp.rooms";
+    $result = $conn->query($query);
 
-        $rooms = [];
-    
-        if ($result->num_rows > 0){
+    $rooms = [];
 
-            while ($row = $result->fetch_assoc()) {
-                $rooms[] = $row;
-            }
-        }else{
-            echo "No rooms founded";
-            return array();
+    if ($result->num_rows > 0) {
+
+        while ($row = $result->fetch_assoc()) {
+            $rooms[] = $row;
         }
-
-        return $rooms;
+    } else {
+        echo "No rooms founded";
+        return array();
     }
 
-    function getOffersRooms() {
-        global $conn;
-        $query = "SELECT * FROM mirandaphp.rooms WHERE discount > 0";
-        $result = $conn -> query($query);
+    return $rooms;
+}
 
-        $rooms = [];
-    
-        if ($result->num_rows > 0){
+function getOffersRooms()
+{
+    global $conn;
+    $query = "SELECT * FROM mirandaphp.rooms WHERE discount > 0";
+    $result = $conn->query($query);
 
-            while ($row = $result->fetch_assoc()) {
-                $rooms[] = $row;
-            }
-        }else{
-            echo "No rooms with offer";
-            return array();
+    $rooms = [];
+
+    if ($result->num_rows > 0) {
+
+        while ($row = $result->fetch_assoc()) {
+            $rooms[] = $row;
+        }
+    } else {
+        echo "No rooms with offer";
+        return array();
+    }
+
+    return $rooms;
+}
+
+
+function getRoomById($roomId)
+{
+    global $conn;
+    $query = "SELECT * FROM mirandaphp.rooms WHERE id = ?";
+    $statement = $conn->prepare($query);
+    $statement->bind_param("i", $roomId);
+    $statement->execute();
+
+    $result = $statement->get_result();
+
+    if ($result->num_rows == 0) {
+        return array();
+    }
+
+    return $result->fetch_assoc();
+}
+
+function reserveRoom()
+{
+    global $conn;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $checkin = $_POST['arrivalDate'];
+        $checkout = $_POST['leDepartureDate'];
+        $fullname = $_POST['fullname'];
+        $email = $_POST['email'];
+        $phone = $_POST['phone'];
+        $message = $_POST['message'];
+        $roomId = $_GET['room_id'];
+
+        $sql = "INSERT INTO mirandaphp.bookings (photo, name, orderDate, orderTime, checkinDate, checkinTime, checkout, checkoutTime, notes, roomId, status)
+            VALUES ('https://picsum.photos/seed/qqzkg4/640/480', ?, CURDATE(),  DATE_FORMAT(NOW(), '%H:%i:%s'), ?, '12:00:00', ?, '12:00:00', ?, ?, 'booked')";
+
+        $statement = $conn->prepare($sql);
+
+        if (!$statement) {
+            die("Error al preparar la consulta: " . $conn->error);
         }
 
-        return $rooms;
-    }
-    
+        $statement->bind_param("ssssi", $fullname, $checkin, $checkout, $message, $roomId);
 
-    function getRoomById($roomId){
-        global $conn;
-        $query = "SELECT * FROM mirandaphp.rooms WHERE id = ?";
-        $statement = $conn->prepare($query);
-        $statement->bind_param("i", $roomId);
-        $statement->execute();
 
-        $result = $statement->get_result();
-
-        if ($result->num_rows == 0){
-            return array();
+        if (!$statement->execute()) {
+            die("Error al ejecutar la consulta: " . $statement->error);
         }
 
-        return $result->fetch_assoc();
+        $statement->close();
+        $conn->close();
     }
+}
 
--- roomDetails.php
+++ roomDetails.php
@@ -2,4 +2,5 @@
 require './config.php';
 require_once "./controllers/rooms.php";
 $room = getRoomById($_GET['room_id']);
+
 echo $blade->run("roomDetails", ['room'=>$room]);
