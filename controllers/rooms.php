ce1c28449e332d11c3360834d843eab5dbd6ff2f
mlevitorresv <levitorres66@gmail.com>
2024-05-07 11:25:47 +0200

    reserve room form

--- controllers/rooms.php
+++ controllers/rooms.php
@@ -61,37 +61,38 @@ function getRoomById($roomId)
     return $result->fetch_assoc();
 }
 
-function reserveRoom()
-{
-    global $conn;
+// function reserveRoom()
+// {
+//     var_dump($_POST);
+//     global $conn;
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $checkin = $_POST['arrivalDate'];
-        $checkout = $_POST['leDepartureDate'];
-        $fullname = $_POST['fullname'];
-        $email = $_POST['email'];
-        $phone = $_POST['phone'];
-        $message = $_POST['message'];
-        $roomId = $_GET['room_id'];
+//     if ($_SERVER["REQUEST_METHOD"] == "POST") {
+//         $checkin = $_POST['arrivalDate'];
+//         $checkout = $_POST['leDepartureDate'];
+//         $fullname = $_POST['fullName'];
+//         $email = $_POST['email'];
+//         $phone = $_POST['phone'];
+//         $message = $_POST['message'];
+//         $roomId = $_POST['room'];
 
-        $sql = "INSERT INTO mirandaphp.bookings (photo, name, orderDate, orderTime, checkinDate, checkinTime, checkout, checkoutTime, notes, roomId, status)
-            VALUES ('https://picsum.photos/seed/qqzkg4/640/480', ?, CURDATE(),  DATE_FORMAT(NOW(), '%H:%i:%s'), ?, '12:00:00', ?, '12:00:00', ?, ?, 'booked')";
+//         $sql = "INSERT INTO mirandaphp.bookings (photo, name, orderDate, orderTime, checkinDate, checkinTime, checkout, checkoutTime, notes, roomId, status)
+//             VALUES ('https://picsum.photos/seed/qqzkg4/640/480', ?, CURDATE(),  DATE_FORMAT(NOW(), '%H:%i:%s'), ?, '12:00:00', ?, '12:00:00', ?, ?, 'booked')";
 
-        $statement = $conn->prepare($sql);
+//         $statement = $conn->prepare($sql);
 
-        if (!$statement) {
-            die("Error al preparar la consulta: " . $conn->error);
-        }
+//         if (!$statement) {
+//             die("Error al preparar la consulta: " . $conn->error);
+//         }
 
-        $statement->bind_param("ssssi", $fullname, $checkin, $checkout, $message, $roomId);
+//         $statement->bind_param("ssssi", $fullname, $checkin, $checkout, $message, $roomId);
 
 
-        if (!$statement->execute()) {
-            die("Error al ejecutar la consulta: " . $statement->error);
-        }
+//         if (!$statement->execute()) {
+//             die("Error al ejecutar la consulta: " . $statement->error);
+//         }
 
-        $statement->close();
-        $conn->close();
-    }
-}
+//         $statement->close();
+//         $conn->close();
+//     }
+// }
 
--- roomDetails.php
+++ roomDetails.php
@@ -3,4 +3,33 @@ require './config.php';
 require_once "./controllers/rooms.php";
 $room = getRoomById($_GET['room_id']);
 
-echo $blade->run("roomDetails", ['room'=>$room]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $checkin = $_POST['arrivalDate'];
+    $checkout = $_POST['leDepartureDate'];
+    $fullname = $_POST['fullName'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $message = $_POST['message'];
+    $roomId = $_POST['room'];
+
+    $sql = "INSERT INTO mirandaphp.bookings (photo, name, orderDate, orderTime, checkinDate, checkinTime, checkout, checkoutTime, notes, roomId, status)
+        VALUES ('https://picsum.photos/seed/qqzkg4/640/480', ?, CURDATE(),  DATE_FORMAT(NOW(), '%H:%i:%s'), ?, '12:00:00', ?, '12:00:00', ?, ?, 'booked')";
+
+    $statement = $conn->prepare($sql);
+
+    if (!$statement) {
+        die("Error al preparar la consulta: " . $conn->error);
+    }
+
+    $statement->bind_param("ssssi", $fullname, $checkin, $checkout, $message, $roomId);
+
+
+    if (!$statement->execute()) {
+        die("Error al ejecutar la consulta: " . $statement->error);
+    }
+
+    $statement->close();
+    $conn->close();
+}
+
+echo $blade->run("roomDetails", ['room' => $room]);
