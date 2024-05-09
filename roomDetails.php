9c28cd759e1aa0116c45542067d4404a875cfc9c
mlevitorresv <levitorres66@gmail.com>
2024-05-09 10:12:46 +0200

    alert when sends reserve form

--- roomDetails.php
+++ roomDetails.php
@@ -4,13 +4,13 @@ require_once "./controllers/rooms.php";
 $room = getRoomById($_GET['room_id']);
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $checkin = $_POST['arrivalDate'];
-    $checkout = $_POST['leDepartureDate'];
-    $fullname = $_POST['fullName'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $message = $_POST['message'];
-    $roomId = $_POST['room'];
+    $checkin = htmlspecialchars(trim($_POST['arrivalDate']));
+    $checkout = htmlspecialchars(trim($_POST['leDepartureDate']));
+    $fullname = htmlspecialchars(trim($_POST['fullName']));
+    $email = htmlspecialchars(trim($_POST['email']));
+    $phone = htmlspecialchars(trim($_POST['phone']));
+    $message = htmlspecialchars(trim($_POST['message']));
+    $roomId = htmlspecialchars(trim($_POST['room']));
 
     $sql = "INSERT INTO mirandaphp.bookings (photo, name, orderDate, orderTime, checkinDate, checkinTime, checkout, checkoutTime, notes, roomId, status)
         VALUES ('https://picsum.photos/seed/qqzkg4/640/480', ?, CURDATE(),  DATE_FORMAT(NOW(), '%H:%i:%s'), ?, '12:00:00', ?, '12:00:00', ?, ?, 'booked')";
@@ -30,6 +30,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $statement->close();
     $conn->close();
+
+    echo '<script type="text/javascript">
+        alert("Reserva realizada ¡Gracias!");
+        </script>';
 }
 
 echo $blade->run("roomDetails", ['room' => $room]);
